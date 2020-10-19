fa5e544019233b20f8ae9bf0ca085ef7d002a566
katerynakofanova <katerynakofanova@gmail.com>
2020-10-19 16:51:55 +0300

    task 4.16 done

--- bloglist/controllers/users.js
+++ bloglist/controllers/users.js
@@ -4,6 +4,12 @@ const User = require('../models/user')
 
 usersRouter.post('/', async(request, response) => {
     const body = request.body
+    if (body.password === undefined){
+        return response.status(400).json({ error: 'password is missing' })
+    } else if (body.password.length < 3 ) {
+        return response.status(400).json({ error: 'password is too short' })
+    }
+    
     const saltRouds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRouds)
 
